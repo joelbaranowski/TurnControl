4e8a4dcdd4ff1444c7731bd65660e635e7979ba6
xelliotpenguin <zhenghuan.zhao@gmail.com>
2014-04-22 15:20:32 -0500

    enhanced front-end UI: added the ability to choose which subgame to join in a dialog box

---
+++ src/servlets/GetGameListServlet.java
@@ -0,0 +1,51 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Query;
+import com.google.gson.Gson;
+
+public class GetGameListServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 4564250159863375259L;
+	DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+	private Gson gson = new Gson();
+
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		Key gameKey = KeyFactory.createKey("RegisterGameKey", "GameList");
+		Query query = new Query("RegisterGame", gameKey);
+		List<Entity> gameList = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(100));
+		List<String> ret = new ArrayList<String>();
+		for (Entity e : gameList) {
+			ret.add((String) e.getProperty("url"));
+		}
+		resp.getWriter().println(gson.toJson(ret));	
+	}
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		Key gameKey = KeyFactory.createKey("RegisterGameKey", "GameList");
+		Query query = new Query("RegisterGame", gameKey);
+		List<Entity> gameList = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(100));
+		List<String> ret = new ArrayList<String>();
+		for (Entity e : gameList) {
+			ret.add((String) e.getProperty("url"));
+		}
+		resp.getWriter().println(gson.toJson(ret));	
+	}
+
+}
