96b836769ce690d866e14f76b2a256003daddfc5
joelbaranowski <joelbaranowski@yahoo.com>
2014-04-25 20:53:52 -0500

    JoinGame changes

--- src/servlets/JoinGameServlet.java
+++ src/servlets/JoinGameServlet.java
@@ -37,55 +37,38 @@ public class JoinGameServlet extends HttpServlet {
 		// Parse request 
 		BufferedReader br = new BufferedReader(new InputStreamReader(req.getInputStream()));    
 		JoinGame request = gson.fromJson(br, JoinGame.class);
-
+		doModPost(request, req, resp);
+	}
+	
+	public void doModPost(JoinGame request, HttpServletRequest req, HttpServletResponse resp) throws IOException{
 
 		Transaction tx = datastore.beginTransaction();
 		try {
 
-
-			Transaction tx2 = datastore.beginTransaction();
-			//Get the playerId to use for this new player from the datastore
-			//And update the nextId field for future access
-			Key idKey = KeyFactory.createKey("idMakerKey", "PlayerIdGenerator");
-			long newId;
-			Entity a = datastore.get(idKey);
-			newId = (long) a.getProperty("nextId");
-			a.setProperty("nextId",newId+1);
-			datastore.delete(idKey);
-			datastore.put(a);
-			tx2.commit();
-
-
-			//This checks if an existing player has the same name as the
-			//new player from the request, and if so deletes the existing entry
-			//Remove if you want multiple players with the same name
+			Long maxID = -1L;
+			Long newID = -1L;
+			
 			Key playerKey = KeyFactory.createKey("JoinGameKey", "PlayerList");
 			Query query = new Query("JoinGame", playerKey);
-			boolean foundMatch = false;
-
 			List<Entity> playerList = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(100));
-			for(Entity e : playerList)
-				if(((String)e.getProperty("playerName")).equals(request.playerName)){
-					foundMatch = true;
-					StatusResponse reponse = new StatusResponse("fail", "already found a match");
-					resp.getWriter().println(reponse.toJson());
-					datastore.delete(e.getKey());
-					break;
+			for(Entity e : playerList){
+				Long currID = (Long)e.getProperty("playerID");
+				if(currID > maxID){
+					maxID = currID;
 				}
+			}
 
-
+			newID = maxID + 1;
 			Entity newPlayer = new Entity("JoinGame", playerKey);
-			newPlayer.setProperty("playerID", newId);
+			newPlayer.setProperty("playerID", newID);
 			newPlayer.setProperty("gameURL", request.gameURL);
 			newPlayer.setProperty("playerName", request.playerName);
 			newPlayer.setProperty("isAI", request.isAI);
 			datastore.put(newPlayer);
 			tx.commit();
 
-			if (!foundMatch) {
-				StatusResponse reponse = new StatusResponse("ok", newId+"");
-				resp.getWriter().println(reponse.toJson());
-			}
+			StatusResponse reponse = new StatusResponse("ok", newID+"");
+			resp.getWriter().println(reponse.toJson());
 
 		}
 		catch(Exception e){
@@ -94,6 +77,5 @@ public class JoinGameServlet extends HttpServlet {
 			ExceptionStringify es = new ExceptionStringify(e);
 			resp.getWriter().print(es.run());
 		}
-
 	}
 }
